8139fce79c88f37efed241b8e21bc94125c2763e
muhammadaqilp <muhammadpratama1816@gmail.com>
2021-03-07 13:34:09 +0700

    menambahkan respon update progress

--- main.py
+++ main.py
@@ -164,16 +164,19 @@ def isUserin(uname, password):
 
 @app.route('/updatebaca/<idpinjam>/<progress>')
 def updateProgress(idpinjam, progress):
-    postData().updateProgressBaca(idpinjam, progress)
+    try:
+        postData().updateProgressBaca(idpinjam, progress)
+        result = {"message": "1"}
+    except:
+        result = {"message": "0"}
+
+    return result
 
 @app.route("/listkategori")
 def getlistCat():
     respon = GetData().listKategori()
     respon = {"data" : respon}
     return respon
-
-
-
 
 
 """@app.route("/data/<section>/<apa>")
@@ -184,4 +187,4 @@ def data2(section, apa):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=1298, debug=True)
+    app.run(host='0.0.0.0', debug=True)
