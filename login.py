6074e71070581e9410cd7874f80fdbd385bfd200
chamut <mutisabila@gmail.com>
2021-03-22 14:16:50 +0700

    password storing to db

--- login.py
+++ login.py
@@ -17,6 +17,7 @@ class login:
     def ceklogin(self, username, password):
         mydb = connectdb()
         cursor = mydb.cursor(dictionary=True)
+        password = '$2b$12$cYyLxD1Fh7fksAjP2l5NYO' + password
         cursor.execute("SELECT * FROM user WHERE (username = '{uname}') AND (password = '{pwd}')".format(uname=username, pwd=password))
         result = cursor.fetchall()
         cursor.close()
